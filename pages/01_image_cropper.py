fa5ef3e111450a8a77dad8f21f60303aa6cfcc8f
victorriapopova <99605102+victorriapopova@users.noreply.github.com>
2025-03-01 18:43:13 +0100

    Update 01_image_cropper.py

--- pages/01_image_cropper.py
+++ pages/01_image_cropper.py
@@ -1,59 +1,49 @@
-import streamlit as st
 import numpy as np
+import streamlit as st
 from PIL import Image
-import os
 import matplotlib.pyplot as plt
+
 
 img_path = "data/starry_night.png"
 
-# Открываем изображение и конвертируем в массив numpy
 with Image.open(img_path) as img:
     img_arr = np.array(img)
 
-# Отображаем изображение
-plt.imshow(img_arr)
-plt.axis('off')  # Это выключает оси, если не хотите их видеть
+'Before cropping'
+
 st.image(img_arr)
 
-# Ex 1.1: Get the height and width of the image in pixels
+max_height = img_arr.shape[0]
+max_width = img_arr.shape[1]
 
-# Get the height and width of the image in pixels
-max_height, max_width = img_arr.shape[:2]  # shape[0] is height, shape[1] is width
-
-# Print the height and width
 f"Height: {max_height} pixels"
 f"Width: {max_width} pixels"
 
-# Ex 1.2: Define the dimensions of the crop, make sure that min is smaller than max and also that min is larger than 0
-# and max is smaller than the size of the image for every dimension. If not, print a message explaining the error.
 
-# Example crop dimensions, modify them to test your code
 crop_min_h = 600
 crop_max_h = 1000
 crop_min_w = 550
 crop_max_w = 1100
 
-# Constraint checks
+if (crop_min_h < 0) or (crop_min_w < 0):
+    print('Min value has to be > 0')
 
-if not (0 < crop_min_h < crop_max_h < max_height):
-    print(f"Error: Crop height boundaries are invalid. Ensure 0 < min_h < max_h < {max_height}.")
-elif not (0 < crop_min_w < crop_max_w < max_width):
-    print(f"Error: Crop width boundaries are invalid. Ensure 0 < min_w < max_w < {max_width}.")
-elif crop_min_h >= crop_max_h:
-    print("Error: crop_min_h should be smaller than crop_max_h.")
-elif crop_min_w >= crop_max_w:
-    print("Error: crop_min_w should be smaller than crop_max_w.")
-else:
-    print("Crop dimensions are valid.")
+if crop_max_h > max_height:
+    print(f'Max crop height has to be less than {max_height}') 
 
+if crop_max_w > max_width:
+    print(f'Max crop width has to be less than {max_width}')
 
-# TODO: Develop the code to validate the constraints and print the error message if any constraint is invalid
-
-# Ex 1.3: Generate the crop array into a new variable, use NumPy array slicing
+f'Crop dimensions (h:h,w:w): {crop_min_h}:{crop_max_h}, {crop_min_w}:{crop_max_w}'
 
 crop_arr = img_arr[crop_min_h:crop_max_h, crop_min_w:crop_max_w]
+
 crop_img = Image.fromarray(crop_arr)
-plt.imshow(crop_arr)
-st.image(crop_arr)
-file_name = "starry_night_cropped"
-crop_img.save(f"/data/{file_name}.png")
+
+file_name = 'cropped_starry_night'
+
+crop_img.save(f"data/{file_name}.png")
+
+'After cropping'
+
+st.image(crop_img)
